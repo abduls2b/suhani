28738c0f7ecdbe6bc44a5fc5568aee8c0ec945f4
abduls2b <122350882+abduls2b@users.noreply.github.com>
2026-06-01 19:44:09 +0530

    Update index.php

--- app/jioweb/index.php
+++ app/jioweb/index.php
@@ -201,9 +201,9 @@ $context = stream_context_create([
   ]
 ]);
 
-//$m3uContent = file_get_contents("https://raw.githubusercontent.com/alex8875/m3u/refs/heads/main/jtv.m3u", false, $context);
+$m3uContent = file_get_contents("https://raw.githubusercontent.com/abduls2b/suhani/refs/heads/main/playlist.m3u", false, $context);
 
-$m3uContent = file_get_contents("playlist.m3u", false, $context);
+//$m3uContent = file_get_contents("playlist.m3u", false, $context);
 
 
 $channels = [];
